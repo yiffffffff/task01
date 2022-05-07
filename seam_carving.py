8ff3869f0ddc6693bcdc5d0e7788ccb45b235059
yf <1339218487@qq.com>
2022-05-07 22:04:35 +0800

    增加了放大图像

--- seam_carving.py
+++ seam_carving.py
@@ -52,14 +52,28 @@ def crop_c(img, scale_c):
     new_c = int(scale_c * c)
 
     #这里用了显示进度条的库
-    for i in trange(c - new_c): 
-        img = carve_column(img)
+    for i in trange(c - new_c):
+        img=carve_column(img)
+
+    return img
+
+def add_c(img, scale_c):
+    r, c, _ = img.shape
+    new_c = int(scale_c * c)
+
+    img=add_column(img,new_c-c)
 
     return img
 
 def crop_r(img, scale_r):
     img = np.rot90(img, 1, (0, 1))
     img = crop_c(img, scale_r)
+    img = np.rot90(img, 3, (0, 1))
+    return img
+
+def add_r(img, scale_r):
+    img = np.rot90(img, 1, (0, 1))
+    img = add_c(img, scale_r)
     img = np.rot90(img, 3, (0, 1))
     return img
 
@@ -78,6 +92,45 @@ def carve_column(img):
     img = img[mask].reshape((r, c - 1, 3))
     return img
 
+def add_seam(im, seam_idx):
+    h, w = im.shape[:2]
+    output = np.zeros((h, w + 1, 3))
+    for row in range(h):
+        col = seam_idx[row]
+        for ch in range(3):
+            if col == 0:
+                p = np.average(im[row, col: col + 2, ch])
+                output[row, col, ch] = im[row, col, ch]
+                output[row, col + 1, ch] = p
+                output[row, col + 1:, ch] = im[row, col:, ch]
+            else:
+                p = np.average(im[row, col - 1: col + 1, ch])
+                output[row, : col, ch] = im[row, : col, ch]
+                output[row, col, ch] = p
+                output[row, col + 1:, ch] = im[row, col:, ch]
+
+    return output
+
+def add_column(img,add_c):
+    r,c,channels=img.shape
+    M, backtrack = minimumEnergySeam(img)
+    seams_record = np.ones((add_c,r),dtype=np.int)
+    jmax=np.argmax(M[-1])
+    for i in range(add_c):
+        j = np.argmin(M[-1])
+        M[-1,j]=M[-1,jmax]
+        seam=np.ones(r,dtype=np.int)
+        for i2 in reversed(range(r)):
+            seam[i2]=j
+            j=backtrack[i2,j]
+        seams_record[i]=seam
+    for i in trange(add_c):
+        img=add_seam(img,seams_record[i])
+        for i2 in range(i+1,add_c):
+            seams_record[i2][np.where(seams_record[i2]>=seams_record[i])]+=1
+
+    img = add_seam(img, seam)
+    return img
 
 def minimumEnergySeam(img):
     #shape 能返回图片的宽高和通道数，
@@ -106,36 +159,6 @@ def minimumEnergySeam(img):
             M[i,j]+=min_energy
     return M,backtrack
 
-
-def minimumEnergySeam2(img):
-    #shape 能返回图片的宽高和通道数，
-    r,c,channels=img.shape
-    energy_map=calc_energy(img)
-
-    M=energy_map.copy()
-    backtrack = np.zeros_like(M, dtype=np.int) #创建一个同大小，但初始化为0的数组
-
-    for i in range(1,r):
-        for j in range(0,c):
-            #处理左侧边缘
-            if j==0:
-                idx=np.argmin(M[i-1,j:j+2])
-                backtrack[i,j]=idx+j
-                min_energy=M[i-1,idx+j]
-            #处理右侧边缘
-            elif j==c-1:
-                idx=idx = np.argmin(M[i - 1, j - 1:j + 1])
-                backtrack[i, j] = idx + j - 1
-                min_energy = M[i - 1, idx + j - 1]
-            else:
-                idx=np.argmin(M[i-1,j-1:j+2])
-                backtrack[i,j]=idx+j-1
-                min_energy=M[i-1,idx+j-1]
-            M[i,j]+=min_energy
-    return M,backtrack
-
-
-
 def main():
     if len(sys.argv) != 5:
         print('usage: carver.py <r/c> <scale> <image_in> <image_out>', file=sys.stderr)
@@ -148,13 +171,22 @@ def main():
 
     img = imread(in_filename)
 
-    if which_axis == 'r':
-        out = crop_r(img, scale)
-    elif which_axis == 'c':
-        out = crop_c(img, scale)
-    else:
-        print('usage: carver.py <r/c> <scale> <image_in> <image_out>', file=sys.stderr)
-        sys.exit(1)
+    if scale<1:
+        if which_axis == 'r':
+            out = crop_r(img, scale)
+        elif which_axis == 'c':
+            out = crop_c(img, scale)
+        else:
+            print('usage: carver.py <r/c> <scale> <image_in> <image_out>', file=sys.stderr)
+            sys.exit(1)
+    elif scale>1:
+        if which_axis == 'r':
+            out = add_r(img, scale)
+        elif which_axis == 'c':
+            out = add_c(img, scale)
+        else:
+            print('usage: carver.py <r/c> <scale> <image_in> <image_out>', file=sys.stderr)
+            sys.exit(1)
     imwrite(out_filename, out)
 
 if __name__ == '__main__':
